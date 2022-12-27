e3a4e5d6c79c9d01c9e0dee65d056608d3a38aed
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-12-27 15:20:57 -0300

    início do módulo de tipografia

---
+++ 08_tipografia/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        arial: 'Arial',
+      },
+      fontSize: {
+        '12xl': '8rem'
+      },
+    },
+  },
+  plugins: [],
+}
